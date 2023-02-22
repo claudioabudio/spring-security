e41b3ac35ff460d1bf178b606369ec5d81890f8f
claudio Abud <clau@gmail.com>
2023-02-22 10:22:17 -0600

    Example using custom AuthenticationProvider

---
+++ ssia-ch2-ex5/src/test/java/org/claudio/ssiach2ex5/SsiaCh2Ex5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.claudio.ssiach2ex5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh2Ex5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
